db23aa365e46c97d6ef2aadc001c814e08c8c189
grizz03 <sawilson1110@yahoo.com>
2021-07-28 12:16:07 -0400

    changing text color

--- app/src/main/java/com/stephenw/rvnum/main/NumAdapter.kt
+++ app/src/main/java/com/stephenw/rvnum/main/NumAdapter.kt
@@ -1,14 +1,24 @@
 package com.stephenw.rvnum.main
 
 import android.graphics.Color
+import android.os.CountDownTimer
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.stephenw.rvnum.R
 import com.stephenw.rvnum.databinding.ItemLayoutBinding
 
 class NumAdapter(private var list: MutableList<Int>) :
     RecyclerView.Adapter<NumAdapter.ViewHolder>() {
+
+//    var handler: Handler = Handler()
+//    var runnable: Runnable? = null
+//    var delay = 5000
 
     inner class ViewHolder(private val binding: ItemLayoutBinding) :
         RecyclerView.ViewHolder(binding.root) {
@@ -33,14 +43,24 @@ class NumAdapter(private var list: MutableList<Int>) :
             }
         }
 
+//        fun onResume() {
+//            handler.postDelayed(Runnable {
+//                handler.postDelayed(runnable!!, delay.toLong())
+//                changeBackground()
+//            }.also { runnable = it }, delay.toLong())
+//        }
+
         private fun changeBackground() {
             val numRange = 0..255
             val randomNum1: Int = numRange.random()
             val randomNum2: Int = numRange.random()
             val randomNum3: Int = numRange.random()
             binding.listItemTv.setBackgroundColor(Color.rgb(randomNum1, randomNum2, randomNum3))
+            binding.listItemTv.setTextColor(Color.rgb(randomNum3, randomNum1, randomNum2))
+
         }
     }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = ItemLayoutBinding.inflate(
